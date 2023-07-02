1c3b05d4e7b45a4d9fc04fd626ebaccd9bcea577
Jdoggett50 <dillondoggett5@gmail.com>
2023-07-02 08:32:56 -0400

    layed out contents, added notes for reference

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+
+//  instantiated book logic
 //initialize empty array to store books
+
+//  styling logic
+//when the array.length is < 0 add books will appear in the center of the page
+//where the books will reside
+//when the array.length > 0, remove the text in the middle of the screen and place the
+//add button to top right side of content
+//with each book, add a photo of a random colored closed book and toggle markup
+//on the  
 
 let myLibrary = [];
 //create the constructor for each book instance
