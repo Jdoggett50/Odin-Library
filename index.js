810f58c7cc7024d97e56075f44d95a07d0ae5d24
Jdoggett50 <dillondoggett5@gmail.com>
2023-07-05 07:21:46 -0400

    added clear button functionality, removed backgrounds

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-//  form generation logic
-//toggle the display property of transparent  
-//background. On add, toggle display: flex;
-//on close, toggle display: none;
+let myLibrary = [];
 
-//target the elements to add event listeners to
-const addBook = document.querySelector('.add-book');
+const addButton = document.querySelector('.add-button');
 const background = document.querySelector('.transparent-background');
 const close = document.querySelector('.close');
-addBook.addEventListener('click', () => background.style.display = 'grid');
+
+addButton.addEventListener('click', () => background.style.display = 'grid');
 close.addEventListener('click',() => background.style.display = 'none');
 
+const createBook = document.querySelector('.submit-book');
 const bookName = document.querySelector('#book-name');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#total-pages');
 
-//book information needs to be save to the object 
-// listen for add book click
-const createBook = document.querySelector('.create-book');
+const clearBook = document.querySelector('.clear-book');
+clearBook.addEventListener('click', () => {
+    //target the input dom elements, remove their .values
+    const inputValues = document.querySelectorAll('label > input')
+    inputValues.forEach((input)=> input.value = '');
+})
+
+
 createBook.addEventListener('click', () => {
     //receive the data from the form
     let anotherBook = new Book(bookName.value,author.value,pages.value);
     myLibrary.push(anotherBook);
+    // myLibrary.forEach(() => {
+    //     const div = document.createElement('div');
+    //     div.style.width = '20px';
+    //     div.style.height = '20px';
+    // })
     console.log(myLibrary);
 })
 
-//  instantiated book logic
-//initialize empty array to store books
-let myLibrary = [];
-//create the constructor for each book instance
+
+//forEach item in the array, create a div, with
+//x height, x width, a link tag with the title
+
 function Book (title,author,pages){
     //instantiate the object every time data is filled out
     this.title = title,
