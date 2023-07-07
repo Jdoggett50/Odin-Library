be84b43a4e9ed92458e2aad49d2ccf917416ef78
Jdoggett50 <dillondoggett5@gmail.com>
2023-07-07 07:13:17 -0400

    added displayBook to context, encountered null error

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const bookName = document.querySelector('#book-name');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#total-pages');
 const clearBook = document.querySelector('.clear-book');
+const bookContainer = document.querySelector('.book-container')
 
 clearBook.addEventListener('click', () => {
     const inputValues = document.querySelectorAll('label > input');
@@ -24,18 +25,26 @@ bookBtn.addEventListener('click', addBook)
 
 function addBook() {
     let anotherBook = new Book(bookName.value,author.value,pages.value);
-    const booksContainer = document.querySelector('.books-container');
-    const div = document.createElement('div');
-    div.classList.add('books');
-    booksContainer.append(div);
     myLibrary.push(anotherBook);
+    //this instance method is equal to 
+    anotherBook.displayBook = displayBook.bind(anotherBook);
+    anotherBook.displayBook();
+    //assign the new function with the 'this' binding to a method derived from a separate function
+    console.log(myLibrary)
 };
 
-//how do I make it so that the book is clickable?
+//create a function that will be used in the forEach method
 
+function displayBook() {
+    const div = document.createElement('div');
+    div.textContent = `${this.title}`;
+    div.classList.add('books')
+    bookContainer.append(div);
+    //add div to classList('books')
+}
 
 function Book (title,author,pages){
-    this.title = title,
-    this.author = author,
-    this.pages = pages
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 };
