044c1c6ff2a1f2d8ead29ebe7372dfc16ce13c77
Jdoggett50 <dillondoggett5@gmail.com>
2023-07-17 07:18:45 -0400

    read button functionality implemented

--- index.js
+++ index.js
@@ -3,43 +3,53 @@ let myLibrary = [];
 const addButton = document.querySelector('.add-button');
 const background = document.querySelector('.transparent-background');
 const close = document.querySelector('.close');
-
-addButton.addEventListener('click', () => background.style.display = 'grid');
-close.addEventListener('click',() => background.style.display = 'none');
-
 const bookBtn = document.querySelector('.submit-book');
 const bookName = document.querySelector('#title');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#total-pages');
 const clearSelection = document.querySelector('.clear-selection');
 const books = document.querySelector('.books');
-const read = document.querySelector('.readButton');
+const readToggle = document.querySelector('.readButton');
 const clearButton = document.querySelector('.remove')
 let booksContainer = document.querySelector('.books-container');
 
+addButton.addEventListener('click', () => background.style.display = 'grid');
+close.addEventListener('click', () => background.style.display = 'none');
+
 clearSelection.addEventListener('click', clearForm);
+
+bookBtn.addEventListener('click', () => {
+    if (bookName.value === '' || author.value === '' || pages.value === '') {
+        alert('Please create a book');
+    } else
+        addBook(bookName.value, author.value, pages.value);
+        // clearForm();
+        displayBooks();
+});
+
+booksContainer.addEventListener('click', (e) => {
+    if (e.target.closest('.remove')) {
+        const onlyRemove = e.target.dataset.item;
+        removeIndex(onlyRemove);
+        displayBooks();
+    } else if (e.target.closest('.read-button')) {
+        // console.log(Book.changeStatus());
+        getStatus(e.target.dataset.status)
+        displayBooks();
+    }
+});
 
 function clearForm() {
     const inputValues = document.querySelectorAll('label > input');
-    inputValues.forEach((input)=> input.value = '');
+    inputValues.forEach((input) => input.value = '');
 }
 
-bookBtn.addEventListener('click', () => {
-    if(bookName.value === '' || author.value === '' || pages.value === ''){
-        alert('Please create a book');
-    } else 
-    addBook(bookName.value, author.value, pages.value, false);
-    // clearForm();
-    displayBooks();
-    console.log(myLibrary);
-});
-
-function addBook(title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read));
+function addBook(title, author, pages) {
+    myLibrary.push(new Book(title, author, pages));
     return myLibrary;
 }
 
-function displayBooks(){
+function displayBooks() {
     booksContainer.textContent = '';
     myLibrary.forEach(index => {
         const divContainer = document.createElement('div');
@@ -58,31 +68,34 @@ function displayBooks(){
         ${index.author} 
         ${index.pages}`;
         readButton.classList.add('read-button');
+        readButton.dataset.status = `${index.title}`
+        if(index.read){
+            readButton.textContent = `Read`;
+        } else
+            readButton.textContent = `Unread`
     });
 };
 
-function Book (title,author,pages,read){
+function getStatus(data) {
+    myLibrary.map(book => {
+        if(book.title === data) {
+            book.read = Book.changeStatus();
+        }
+    })
+}
+
+Book.changeStatus = function () {
+    return this.read = !this.read;
+}
+
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = false;
 };
 
-booksContainer.addEventListener('click', (e)=>{
-    if(e.target.closest('.remove')){
-        const onlyRemove = e.target.dataset.item;
-        removeIndex(onlyRemove);
-        displayBooks()
-    }
-});
-
-function removeIndex(data){
+function removeIndex(data) {
     myLibrary = myLibrary.filter(book => book.title !== data);
     return myLibrary;
 };
-//we are setting the current global myLibrary to myLibrary.filters result
-// and returning the new version of myLibrary to the global myLibrary
-//the myLibrary.filter is looking at each book and comparing book.title
-//to the data attribute on the close buttons. it's saying as long as 
-//the book.title isn't equal to the data attribute on the remove button
-// to return it in the new array that it has created.
